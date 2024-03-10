fb557f14c1c303dd451422f2c42aa83cc14ae9cc
Joni Röntynen <57128169+JoniRont@users.noreply.github.com>
2024-03-10 09:30:10 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,113 @@
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
+import { ExecutionContext } from '@cloudflare/workers-types/experimental';
+import { getAccessToken, getIdToken, verifyIdToken } from 'web-auth-library/google';
 
-/**
- * Associate bindings declared in wrangler.toml with the TypeScript type system
- */
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	TYPE: string;
+	PROJECT_ID: string;
+	PRIVATE_KEY_ID: string;
+	PRIVATE_KEY: string;
+	CLIENT_EMAIL: string;
+	CLIENT_ID: string;
+	AUTH_URI: string;
+	TOKEN_URI: string;
+	AUTH_PROVIDER_X509_CERT_URL: string;
+	CLIENT_X509_CERT_URL: string;
+	UNIVERSE_DOMAIN: string;
+	AUTHORIZATION: string;
 }
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier
-	 *
-	 * @param state - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
-	constructor(state: DurableObjectState, env: Env) {}
+type MessageBody = {
+	message: {
+		token: string;
+		notification: {
+			title: string;
+			body: string;
+		};
+	};
+};
 
-	/**
-	 * The Durable Object fetch handler will be invoked when a Durable Object instance receives a
-	 * 	request from a Worker via an associated stub
-	 *
-	 * @param request - The request submitted to a Durable Object instance from a Worker
-	 * @returns The response to be sent back to the Worker
-	 */
-	async fetch(request: Request): Promise<Response> {
-		return new Response('Hello World');
-	}
-}
+type RequestBody = {
+	tokens: string[];
+	message: {
+		title: string;
+		body: string;
+	};
+};
 
 export default {
 	/**
 	 * This is the standard fetch handler for a Cloudflare Worker
 	 *
 	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
+
 	 * @param ctx - The execution context of the Worker
 	 * @returns The response to be sent back to the client
 	 */
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// We will create a `DurableObjectId` using the pathname from the Worker request
-		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
 
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub: DurableObjectStub = env.MY_DURABLE_OBJECT.get(id);
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response | null> {
+		try {
+			if (request.headers.get('Authorization') !== env.AUTHORIZATION) {
+				console.log('UNAUTHORIZED', JSON.stringify(request.headers, null, 2));
+				return Promise.resolve(Response.json({ statusCode: 401, message: 'UNAUTHORIZED ' }));
+			}
+			if (!request.body) {
+				console.log('Body missing');
+				return Promise.resolve(Response.json({ statusCode: 400, message: 'Body missing' }));
+			}
+			const body = await request.clone().json<RequestBody>();
+			console.log('Start sending message with body', body);
 
-		// We call `fetch()` on the stub to send a request to the Durable Object instance
-		// The Durable Object instance will invoke its fetch handler to handle the request
-		let response = await stub.fetch(request);
-
-		return response;
+			const credentials = {
+				type: env.TYPE,
+				project_id: env.PROJECT_ID,
+				private_key_id: env.PRIVATE_KEY_ID,
+				private_key: env.PRIVATE_KEY,
+				client_email: env.CLIENT_EMAIL,
+				client_id: env.CLIENT_ID,
+				auth_uri: env.AUTH_URI,
+				token_uri: env.TOKEN_URI,
+				auth_provider_x509_cert_url: env.AUTH_PROVIDER_X509_CERT_URL,
+				client_x509_cert_url: env.CLIENT_X509_CERT_URL,
+				universe_domain: env.UNIVERSE_DOMAIN,
+			};
+			const fcmUrlScope = 'https://www.googleapis.com/auth/firebase.messaging';
+			const messagingUrl = `https://fcm.googleapis.com/v1/projects/${env.PROJECT_ID}/messages:send`;
+			console.log(`Send messages to ${messagingUrl}`);
+			const accessToken = await getAccessToken({
+				credentials: credentials,
+				scope: ['https://www.googleapis.com/auth/cloud-platform', fcmUrlScope],
+				waitUntil(promise) {
+					return Promise.resolve();
+				},
+			});
+			console.log(`Access token fetch ${accessToken.length ? 'successfully' : 'failed'}`);
+			const messageResponse = await Promise.all(
+				body.tokens.map((token) =>
+					fetch(messagingUrl, {
+						headers: { Authorization: `Bearer ${accessToken}` },
+						body: JSON.stringify({
+							message: {
+								token: token,
+								notification: {
+									body: body.message.body,
+									title: body.message.title,
+								},
+							},
+						}),
+						method: 'POST',
+					})
+				)
+			);
+			console.log('Sending messages finished');
+			return Promise.resolve(
+				Response.json({
+					failed: messageResponse.filter((i) => i.status !== 200).length,
+					send: messageResponse.filter((i) => i.status === 200).length,
+				})
+			);
+		} catch (err) {
+			console.error('Sending messages failed!', err);
+			return Promise.resolve(Response.json({ statusCode: 500, message: err }));
+		}
 	},
 };
